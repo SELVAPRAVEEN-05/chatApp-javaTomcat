a50074e052c1a886f6c491ed7531d49ee4e40530
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2026-09-21 16:21:25 +0530

    second commit

--- src/main/java/com/chatapp/controller/ChatServlet.java
+++ src/main/java/com/chatapp/controller/ChatServlet.java
@@ -114,7 +114,6 @@ public class ChatServlet extends HttpServlet {
          * 3. GET FILTER
          * =========================================
          * Examples:
-         *
          * /api/chats?filter=all
          * /api/chats?filter=unread
          * /api/chats?filter=read
